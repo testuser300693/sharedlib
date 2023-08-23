a080ad7d0549853913076614d00c00580f08a9d7
testuser300693 <139532198+testuser300693@users.noreply.github.com>
2023-08-23 12:38:10 +0530

    Update SampleClass.groovy

--- src/com/stageclasses/SampleClass.groovy
+++ src/com/stageclasses/SampleClass.groovy
@@ -2,121 +2,121 @@
 
 package com.stageclasses
 
-class SampleClass {
-   String name
-   Integer age
+// class SampleClass {
+//    String name
+//    Integer age
 
-   def increaseAge(Integer years) {
-      this.age += years
-   }
-}
-
-// class PipelineHelper {
-//     String name
-//     Integer age
-//     // def build() {
-//     //     stageCheckout()
-//     //     stageBuild()
-//     //     stageUnitTest()
-//     //     stageStaticAnalysis()
-//     //     stageIntegrationTest()
-//     //     stagePackageAndPublish()
-//     //     stageDeployToStaging()
-//     //     stageFunctionalTesting()
-//     //     stageDeployToProduction()
-//     //     stagePostDeploymentValidation()
-//     // }
-
- 
-//     def increaseAge(Integer years) {
+//    def increaseAge(Integer years) {
 //       this.age += years
 //    }
-//     def stageCheckout() {
-//         stage('Checkout') {
-//             echo "Checking out source code"
-//             // checkout scm
-//         }
-//     }
-
- 
-
-//     // private def stageBuild() {
-//     //     stage('Build') {
-//     //         echo "Building the application"
-//     //         // Build your application here
-//     //     }
-//     // }
-
- 
-
-//     // private def stageUnitTest() {
-//     //     stage('Unit Test') {
-//     //         echo "Running unit tests"
-//     //         // Run unit tests here
-//     //     }
-//     // }
-
- 
-
-//     // private def stageStaticAnalysis() {
-//     //     stage('Static Analysis') {
-//     //         echo "Performing static analysis"
-//     //         // Perform static analysis here
-//     //     }
-//     // }
-
- 
-
-//     // private def stageIntegrationTest() {
-//     //     stage('Integration Test') {
-//     //         echo "Running integration tests"
-//     //         // Run integration tests here
-//     //     }
-//     // }
-
- 
-
-//     // private def stagePackageAndPublish() {
-//     //     stage('Package and Publish') {
-//     //         echo "Packaging and publishing artifacts"
-//     //         // Use the JFrog Artifactory plugin to package and publish artifacts
-//     //         // rtMavenRun(...)
-//     //     }
-//     // }
-
- 
-
-//     // private def stageDeployToStaging() {
-//     //     stage('Deploy to Staging') {
-//     //         echo "Deploying to staging environment"
-//     //         // Deploy to staging environment
-//     //     }
-//     // }
-
- 
-
-//     // private def stageFunctionalTesting() {
-//     //     stage('Functional Testing') {
-//     //         echo "Running functional tests"
-//     //         // Run functional tests here
-//     //     }
-//     // }
-
- 
-
-//     // private def stageDeployToProduction() {
-//     //     stage('Deploy to Production') {
-//     //         echo "Deploying to production environment"
-//     //         // Deploy to production environment
-//     //     }
-//     // }
-
- 
-
-//     // private def stagePostDeploymentValidation() {
-//     //     stage('Post-Deployment Validation') {
-//     //         echo "Performing post-deployment validation"
-//     //         // Perform post-deployment validation here
-//     //     }
-//     // }
 // }
+
+class SampleClass {
+    String name
+    Integer age
+    // def build() {
+    //     stageCheckout()
+    //     stageBuild()
+    //     stageUnitTest()
+    //     stageStaticAnalysis()
+    //     stageIntegrationTest()
+    //     stagePackageAndPublish()
+    //     stageDeployToStaging()
+    //     stageFunctionalTesting()
+    //     stageDeployToProduction()
+    //     stagePostDeploymentValidation()
+    // }
+
+ 
+    def increaseAge(Integer years) {
+      this.age += years
+   }
+    def stageCheckout() {
+        stage('Checkout') {
+            echo "Checking out source code"
+            // checkout scm
+        }
+    }
+
+ 
+
+    // private def stageBuild() {
+    //     stage('Build') {
+    //         echo "Building the application"
+    //         // Build your application here
+    //     }
+    // }
+
+ 
+
+    // private def stageUnitTest() {
+    //     stage('Unit Test') {
+    //         echo "Running unit tests"
+    //         // Run unit tests here
+    //     }
+    // }
+
+ 
+
+    // private def stageStaticAnalysis() {
+    //     stage('Static Analysis') {
+    //         echo "Performing static analysis"
+    //         // Perform static analysis here
+    //     }
+    // }
+
+ 
+
+    // private def stageIntegrationTest() {
+    //     stage('Integration Test') {
+    //         echo "Running integration tests"
+    //         // Run integration tests here
+    //     }
+    // }
+
+ 
+
+    // private def stagePackageAndPublish() {
+    //     stage('Package and Publish') {
+    //         echo "Packaging and publishing artifacts"
+    //         // Use the JFrog Artifactory plugin to package and publish artifacts
+    //         // rtMavenRun(...)
+    //     }
+    // }
+
+ 
+
+    // private def stageDeployToStaging() {
+    //     stage('Deploy to Staging') {
+    //         echo "Deploying to staging environment"
+    //         // Deploy to staging environment
+    //     }
+    // }
+
+ 
+
+    // private def stageFunctionalTesting() {
+    //     stage('Functional Testing') {
+    //         echo "Running functional tests"
+    //         // Run functional tests here
+    //     }
+    // }
+
+ 
+
+    // private def stageDeployToProduction() {
+    //     stage('Deploy to Production') {
+    //         echo "Deploying to production environment"
+    //         // Deploy to production environment
+    //     }
+    // }
+
+ 
+
+    // private def stagePostDeploymentValidation() {
+    //     stage('Post-Deployment Validation') {
+    //         echo "Performing post-deployment validation"
+    //         // Perform post-deployment validation here
+    //     }
+    // }
+}
