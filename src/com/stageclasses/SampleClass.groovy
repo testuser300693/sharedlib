6ece94746944c791734d460936b9b82a022fd5b5
testuser300693 <139532198+testuser300693@users.noreply.github.com>
2023-08-23 12:25:53 +0530

    Update SampleClass.groovy

--- src/com/stageclasses/SampleClass.groovy
+++ src/com/stageclasses/SampleClass.groovy
@@ -1,20 +1,20 @@
 class PipelineHelper {
-    def build() {
-        stageCheckout()
-        stageBuild()
-        stageUnitTest()
-        stageStaticAnalysis()
-        stageIntegrationTest()
-        stagePackageAndPublish()
-        stageDeployToStaging()
-        stageFunctionalTesting()
-        stageDeployToProduction()
-        stagePostDeploymentValidation()
-    }
+    // def build() {
+    //     stageCheckout()
+    //     stageBuild()
+    //     stageUnitTest()
+    //     stageStaticAnalysis()
+    //     stageIntegrationTest()
+    //     stagePackageAndPublish()
+    //     stageDeployToStaging()
+    //     stageFunctionalTesting()
+    //     stageDeployToProduction()
+    //     stagePostDeploymentValidation()
+    // }
 
  
 
-    private def stageCheckout() {
+    def stageCheckout() {
         stage('Checkout') {
             echo "Checking out source code"
             // checkout scm
@@ -23,83 +23,83 @@ class PipelineHelper {
 
  
 
-    private def stageBuild() {
-        stage('Build') {
-            echo "Building the application"
-            // Build your application here
-        }
-    }
+    // private def stageBuild() {
+    //     stage('Build') {
+    //         echo "Building the application"
+    //         // Build your application here
+    //     }
+    // }
 
  
 
-    private def stageUnitTest() {
-        stage('Unit Test') {
-            echo "Running unit tests"
-            // Run unit tests here
-        }
-    }
+    // private def stageUnitTest() {
+    //     stage('Unit Test') {
+    //         echo "Running unit tests"
+    //         // Run unit tests here
+    //     }
+    // }
 
  
 
-    private def stageStaticAnalysis() {
-        stage('Static Analysis') {
-            echo "Performing static analysis"
-            // Perform static analysis here
-        }
-    }
+    // private def stageStaticAnalysis() {
+    //     stage('Static Analysis') {
+    //         echo "Performing static analysis"
+    //         // Perform static analysis here
+    //     }
+    // }
 
  
 
-    private def stageIntegrationTest() {
-        stage('Integration Test') {
-            echo "Running integration tests"
-            // Run integration tests here
-        }
-    }
+    // private def stageIntegrationTest() {
+    //     stage('Integration Test') {
+    //         echo "Running integration tests"
+    //         // Run integration tests here
+    //     }
+    // }
 
  
 
-    private def stagePackageAndPublish() {
-        stage('Package and Publish') {
-            echo "Packaging and publishing artifacts"
-            // Use the JFrog Artifactory plugin to package and publish artifacts
-            // rtMavenRun(...)
-        }
-    }
+    // private def stagePackageAndPublish() {
+    //     stage('Package and Publish') {
+    //         echo "Packaging and publishing artifacts"
+    //         // Use the JFrog Artifactory plugin to package and publish artifacts
+    //         // rtMavenRun(...)
+    //     }
+    // }
 
  
 
-    private def stageDeployToStaging() {
-        stage('Deploy to Staging') {
-            echo "Deploying to staging environment"
-            // Deploy to staging environment
-        }
-    }
+    // private def stageDeployToStaging() {
+    //     stage('Deploy to Staging') {
+    //         echo "Deploying to staging environment"
+    //         // Deploy to staging environment
+    //     }
+    // }
 
  
 
-    private def stageFunctionalTesting() {
-        stage('Functional Testing') {
-            echo "Running functional tests"
-            // Run functional tests here
-        }
-    }
+    // private def stageFunctionalTesting() {
+    //     stage('Functional Testing') {
+    //         echo "Running functional tests"
+    //         // Run functional tests here
+    //     }
+    // }
 
  
 
-    private def stageDeployToProduction() {
-        stage('Deploy to Production') {
-            echo "Deploying to production environment"
-            // Deploy to production environment
-        }
-    }
+    // private def stageDeployToProduction() {
+    //     stage('Deploy to Production') {
+    //         echo "Deploying to production environment"
+    //         // Deploy to production environment
+    //     }
+    // }
 
  
 
-    private def stagePostDeploymentValidation() {
-        stage('Post-Deployment Validation') {
-            echo "Performing post-deployment validation"
-            // Perform post-deployment validation here
-        }
-    }
+    // private def stagePostDeploymentValidation() {
+    //     stage('Post-Deployment Validation') {
+    //         echo "Performing post-deployment validation"
+    //         // Perform post-deployment validation here
+    //     }
+    // }
 }
